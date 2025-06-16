61f178cf369dc752ddaa6fbf8ba56c6ad1399310
IDjinn <42049518+IDjinn@users.noreply.github.com>
2025-06-16 13:08:17 -0300

    remove TBase

--- src/main/java/idjinn/tools/conditions/Condition.java
+++ src/main/java/idjinn/tools/conditions/Condition.java
@@ -1,18 +1,19 @@
 package idjinn.tools.conditions;
 
 import idjinn.tools.events.Event;
-import idjinn.tools.triggers.TBase;
 import idjinn.tools.triggers.Trigger;
+import lombok.Data;
 import lombok.ToString;
 
 import java.util.List;
 
+@Data
 public abstract class Condition {
     private final String name;
     private final List<Node> nodes;
 
     @ToString.Exclude
-    private transient TBase trigger;
+    private transient Trigger trigger;
 
     public Condition(final String name, final List<Node> nodes) {
         this.name = name;
@@ -24,8 +25,4 @@ public abstract class Condition {
     }
 
     public abstract int type();
-
-    public void setTrigger(final Trigger trigger) {
-        this.trigger = trigger;
-    }
 }
--- src/main/java/idjinn/tools/triggers/Trigger.java
+++ src/main/java/idjinn/tools/triggers/Trigger.java
@@ -1,29 +1,71 @@
 package idjinn.tools.triggers;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import idjinn.tools.actions.Action;
+import idjinn.tools.conditions.Condition;
 import idjinn.tools.events.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Trigger extends TBase {
+public class Trigger {
     private static final Logger log = LoggerFactory.getLogger(Trigger.class);
 
+    private final int id;
+    private final int type;
+    private final String name;
+
+    private final Multimap<Integer, Condition> conditions;
+    private final Multimap<Integer, Action> actions;
+    private final Multimap<Integer, Event> events;
+
     public Trigger(final int id, final int type, final String name) {
-        super(id, type, name);
+        this.id = id;
+        this.type = type;
+        this.name = name;
+
+        this.conditions = HashMultimap.create();
+        this.actions = HashMultimap.create();
+        this.events = HashMultimap.create();
+    }
+
+    public int id() {
+        return this.id;
+    }
+
+    public int type() {
+        return this.type;
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    public void addEvent(final Event event) {
+        this.events.put(event.getType(), event);
+    }
+
+    public void addCondition(final Condition condition) {
+        this.conditions.put(condition.type(), condition);
+    }
+
+    public void addAction(final Action action) {
+        this.actions.put(action.type(), action);
     }
 
     public void process(final Event event) {
         final var startTime = System.currentTimeMillis();
-        for (final var e : this.getEvents().values()) {
+        for (final var e : this.events.values()) {
             if (e.getType() != event.getType()) continue;
 
-            for (final var condition : this.getConditions().values()) {
+            for (final var condition : this.conditions.values()) {
                 final var conditionMatch = condition.process(event);
                 log.trace("event: {}, condition: {}, match: {}", event, condition.type(), conditionMatch);
                 if (!conditionMatch) return;
             }
         }
 
-        for (final var action : this.getActions().values()) {
+        for (final var action : this.actions.values()) {
             log.trace("event: {}, action: {}", event, action);
             action.process(event);
         }
